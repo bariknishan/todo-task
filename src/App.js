d564cd9b7be8d7c15dc516816e0858ec9ea3664c
bariknishan <barik6286@gmail.com>
2022-07-01 01:16:22 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from './Pages/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Task from './Pages/Home/Task';
+import ToDo from './Pages/Home/ToDo';
+import Calender from './Pages/Home/Calender';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-one' >
+     <Navbar></Navbar>
+     
+    <Routes>
+      <Route path='/home'  element={<Home></Home>}></Route>
+      <Route path='/task'  element={<Task></Task>}></Route>
+      <Route path='/todo'  element={<ToDo></ToDo>}></Route>
+      <Route path='/calender'  element={<Calender></Calender>}></Route>
+    </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Task.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Task = () => {
+    return (
+        <div>
+            <h1>Coplelte task</h1>
+        </div>
+    );
+};
+
+export default Task;
